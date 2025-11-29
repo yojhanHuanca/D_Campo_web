7bf217a178318b305c99beda5e2350790163a3bf
b127 <xiomara_mercedes_123@hotmail.com>
2025-11-29 10:55:10 -0500

    Correccion de iconos y frase

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -72,7 +72,7 @@
             <div class="col-12 col-md-6 col-lg-3">
                 <div class="bg-white p-4 rounded-4 shadow-sm h-100">
                     <div class="icon-square bg-success bg-opacity-10 rounded-3 d-flex justify-content-center align-items-center mb-3" style="width: 70px; height:70px; margin:auto;">
-                        <i class="bi bi-leaf text-success fs-1"></i>
+                        <i class="bi bi-tree fs-2 text-success"></i>
                     </div>
                     <h5>Sostenibilidad</h5>
                     <p class="text-muted">Cuidamos el medio ambiente en cada etapa de producción</p>
